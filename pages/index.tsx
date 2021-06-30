0f367986c9945cf98b7e222069cf306728cc67dd
Matias Herranz <matiasherranz@gmail.com>
2021-06-30 17:39:28 -0300

    Initial landing page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,211 +1,229 @@
+import { useEffect } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
+import Router from 'next/router'
 
-export const Home = (): JSX.Element => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import { SPOTIFY_AUTH_URL } from '../utils/constants'
+import { getUrlParams } from '../utils'
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+export const Home = (): JSX.Element => {
+  useEffect(() => {
+    let token = getFromCookie()
+    const urlParams = getUrlParams()
 
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
+    // Clean the URL bar to avoid displaying the access token to the user
+    window.location.hash = ''
 
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
+    if (!token && urlParams.access_token) {
+      token = urlParams.access_token
+      saveCookie(token)
+    }
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+    if (token) {
+      // User has a token (either from a cookie or url param),
+      // let's take them to the app
+      Router.push('/dashboard')
+    }
+  }, [])
+
+  // User does not have a token, let's show them the landing / login button
+  return (
+    <div className="container">
+      <Head>
+        <title>Spotify Playlist App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Welcome to the{' '}
+          <a
+            href="https://open.spotify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Spotify
+          </a>{' '}
+          Playlist Playground!
+        </h1>
+
+        <p className="description">
+          To get started, go ahead and login by clicking the button below.
+        </p>
+
+        <a className="btn btn--loginApp-link" href={SPOTIFY_AUTH_URL}>
+          Login to Spotify
         </a>
+      </main>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
+      <footer>
         <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
+          href="https://www.linkedin.com/in/matiasherranz/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
+          Created by Matías Herranz, powered by
+          <Image
+            src="/nextjs.svg"
+            alt="NextJS logo"
+            height={'32'}
+            width={'64'}
+          />
+          and ReactJS
+          <div style={{ marginLeft: '4px' }}>
+            <Image
+              src="/react.svg"
+              alt="NextJS logo"
+              height={'26'}
+              width={'26'}
+            />
+          </div>
         </a>
+      </footer>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
           flex-direction: column;
+          justify-content: center;
+          align-items: center;
+
+          /* background: rgb(124, 188, 92);
+        background: linear-gradient(
+          300deg,
+          rgba(124, 188, 92, 0.5536589635854341) 9%,
+          rgba(173, 210, 166, 0.5144432773109244) 69%,
+          rgba(211, 228, 196, 0.4108018207282913) 77%
+        ); */
+          /* background: rgb(124, 188, 92);
+        background: linear-gradient(
+          300deg,
+          rgba(124, 188, 92, 0.5536589635854341) 9%,
+          rgba(173, 210, 166, 0.40800070028011204) 63%,
+          rgba(211, 228, 196, 0.4108018207282913) 77%
+        ); */
+          background: rgb(124, 188, 92);
+          background: linear-gradient(
+            300deg,
+            rgba(124, 188, 92, 0.5536589635854341) 9%,
+            rgba(173, 210, 166, 0.335171568627451) 63%,
+            rgba(211, 228, 196, 0.4108018207282913) 77%
+          );
         }
-      }
-    `}</style>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+        /** Buttons **/
+        .btn {
+          background-color: transparent;
+          border-radius: 2em;
+          border: 0.2em solid #1ecd97;
+          color: #1ecd97;
+          cursor: pointer;
+          font-size: 3vmin;
+          padding: 0.7em 1.5em;
+          text-transform: uppercase;
+          transition: all 0.25s ease;
+        }
+
+        .btn:hover {
+          background: #1ecd97;
+          color: #333;
+        }
+
+        .btn--login {
+          margin: 0 auto;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+          margin-right: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+          max-width: 760px;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Home
