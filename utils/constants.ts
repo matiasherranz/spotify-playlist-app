08d19514e04c0647bcfbd1225e7258ef40d848d7
Matias Herranz <matiasherranz@gmail.com>
2021-07-01 18:03:48 -0300

    Updates to contants file.

--- utils/constants.ts
+++ utils/constants.ts
@@ -8,8 +8,12 @@ export const scopes = [
   'user-top-read',
   'user-modify-playback-state',
 ]
-
+// Spotify tokens expire after one hour
+export const SPOTIFY_TOKEN_EXPIRATION_HOURS = 1
 export const SPOTIFY_AUTH_BASE_URL = 'https://accounts.spotify.com/authorize'
 
 const scopesStr = scopes.join('%20')
 export const SPOTIFY_AUTH_URL = `${SPOTIFY_AUTH_BASE_URL}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopesStr}&response_type=token&show_dialog=true`
+
+export const SPOTIFY_PLAYER_URL = 'https://api.spotify.com/v1/me/player'
+export const POLLING_INTERVAL = 5000 // 5 seconds, in milliseconds
