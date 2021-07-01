bc6c3dda2e2209d184f83bb603d2c6cfe3f04478
Matias Herranz <matiasherranz@gmail.com>
2021-07-01 18:03:37 -0300

    Dashboard WIP.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,27 +1,39 @@
 import { useEffect, useState } from 'react'
 
 import Head from 'next/head'
-import Image from 'next/image'
 import Router from 'next/router'
 
+import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
+import { getCookie } from '../utils/cookieStorage'
 import {
   getFromStorage,
   saveToStorage,
   PlaylistsDataType,
 } from '../utils/sessionStorage'
 
+const defaultPlaylistTitle = 'Select a playlist on the left to see its songs!'
+
 export const Dashboard = (): JSX.Element => {
   const [selectedPlaylist, setSelectedPlaylist] = useState<string>()
   const [playlists, setPlaylists] = useState<PlaylistsDataType>()
+  const [authToken, setAuthToken] = useState<string>()
+
+  useEffect(() => {
+    const token = getCookie('token')
+    setAuthToken(token)
+
+    if (!token) {
+      Router.push('/')
+    }
+  }, [])
 
   useEffect(() => {
     const storedPlaylists = getFromStorage('playlists')
-    debugger
 
     // User arriving to the dashboard without playlists: redirect
     // to the screen that let's the user create the first playlist
-    //if (!playlists) Router.push('/addPlaylist?first=true')
+    if (!storedPlaylists) Router.push('/addPlaylist?first=true')
 
     // User arriving to the dashboard, had some playlists from a
     // previous visit: let's load them from storage
@@ -50,193 +62,62 @@ export const Dashboard = (): JSX.Element => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Sidebar playlists={playlists} removePlaylist={handleRemovePlaylist} />
-      <main>
-        <h1 className="title">
-          Welcome to the <a href="https://open.spotify.com/">Spotify</a>{' '}
-          Playlist Playground!
-        </h1>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <article role="main">
+        <Sidebar
+          playlists={playlists}
+          removePlaylist={handleRemovePlaylist}
+          token={authToken}
+        />
+        <div className="content">
+          <h1>
+            {selectedPlaylist ? selectedPlaylist.title : defaultPlaylistTitle}
+          </h1>
+          <p>
+            Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi
+            welsh onion daikon amaranth tatsoi tomatillo melon azuki bean
+            garlic.
+          </p>
+          <p>
+            Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot
+            courgette tatsoi pea sprouts fava bean collard greens dandelion okra
+            wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+          </p>
         </div>
-      </main>
+      </article>
 
-      <footer>
-        <a
-          href="https://www.linkedin.com/in/matiasherranz/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Created by Matías Herranz, powered by
-          <Image
-            src="/nextjs.svg"
-            alt="NextJS logo"
-            height={'32'}
-            width={'64'}
-          />
-          and ReactJS
-          <Image
-            src="/react.svg"
-            alt="NextJS logo"
-            height={'26'}
-            width={'26'}
-          />
-        </a>
-      </footer>
+      <Footer />
 
       <style jsx>{`
+        * {
+          box-sizing: border-box;
+        }
+
         .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          /* background: rgb(124, 188, 92); */
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
+        article {
+          max-width: 1100px;
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+          min-height: calc(90vh - 50px);
+          padding: 10px;
+          border-radius: 4px;
+          margin: 0 auto;
         }
 
-        footer img {
-          margin-left: 0.5rem;
-          margin-right: 0.5rem;
+        article::after {
+          content: '';
+          clear: both;
+          display: block;
         }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-          max-width: 760px;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .content {
+          float: right;
+          width: 69%;
+          /* color: #ccc; */
+          border-radius: 4px;
+          padding: 10px;
+          /* height: calc(90vh - 100px); */
         }
       `}</style>
 
