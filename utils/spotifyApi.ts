8c91184fa949e16bdfa5a925f71e7219eac7aa16
Matias Herranz <matiasherranz@gmail.com>
2021-07-01 18:03:27 -0300

    Added Spotify SPI integration helper function.

---
+++ utils/spotifyApi.ts
@@ -0,0 +1,31 @@
+import { SPOTIFY_PLAYER_URL } from './constants'
+
+async function getData(url = '', token = '') {
+  const response = await fetch(url, {
+    method: 'GET',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    referrerPolicy: 'no-referrer',
+  })
+  return response.json()
+}
+
+type APIResponseType = {
+  item: Record<string, unknown>
+  is_playing: boolean
+  progress_ms: number
+}
+
+export const getCurrentlyPlaying = async (token: string) => {
+  const data: APIResponseType = await getData(SPOTIFY_PLAYER_URL, token)
+  if (!data) return null
+
+  return {
+    item: data.item,
+    is_playing: data.is_playing,
+    progress_ms: data.progress_ms,
+  }
+}
