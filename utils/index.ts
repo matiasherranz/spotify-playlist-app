365a6633217ea773b14b9898a675e1a41f811d76
Matias Herranz <matiasherranz@gmail.com>
2021-07-02 09:44:41 -0300

    Added a method to generate a pseudo-random string hash.

--- utils/index.ts
+++ utils/index.ts
@@ -11,3 +11,14 @@ export const getUrlParams = (): { [key: string]: string } => {
       return initial
     }, {})
 }
+
+export const generateHash = (length = 10): string => {
+  let result = ''
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
