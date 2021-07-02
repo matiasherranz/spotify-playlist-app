2bc4ec2475ac9f761b221a7b9a40fe0943a7e327
Matias Herranz <matiasherranz@gmail.com>
2021-07-02 09:45:18 -0300

    Updated types and added function to add a new playlist.

--- utils/sessionStorage.ts
+++ utils/sessionStorage.ts
@@ -1,14 +1,18 @@
-export type PlaylistsDataType = [
-  {
-    id: string
-    title: string
-    songs: [
-      {
-        // TODO: complete type here
-      }
-    ]
-  }
-]
+import { generateHash } from '.'
+
+type SongType = {
+  cover: string
+  artist: string
+  title: string
+}
+
+type PlaylistType = {
+  id: string
+  title: string
+  songs: SongType[]
+}
+
+export type PlaylistsDataType = PlaylistType[]
 
 // Saves into localStorage under the provided id
 export const saveToStorage = (id: string, data: PlaylistsDataType): void => {
@@ -21,4 +25,12 @@ export const getFromStorage = (id: string): PlaylistsDataType | null => {
   if (!retrievedObject) return null
 
   return JSON.parse(retrievedObject)
+}
+
+export const addNewPlaylist = (title: string): void => {
+  const emptyPlaylist: PlaylistsDataType = []
+  const playlists = getFromStorage('playlists') ?? emptyPlaylist
+
+  playlists.push({ title, songs: [], id: generateHash() })
+  saveToStorage('playlists', playlists)
 }
